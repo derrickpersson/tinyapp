7be09b9e1adc5ab6ede11956ba8aca79c3183a39
Derrck Persson <derrickpersson@gmail.com>
2017-12-05 17:17:11 -0800

    Working redirect function

---
+++ express_server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+
+
+app.set('view engine', 'ejs');
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get('/urls', function(req, res){
+  let templateVar = { urls : urlDatabase}
+  res.render('urls_index', templateVar);
+});
+
+
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if(longURL){
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("Page Not Found");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get('/urls/:id', function(req, res){
+  let templateVars = {shortURL : req.params.id};
+  res.render('urls_show', templateVars);
+});
+//  URL Shortening functions:
+
+function generateRandomString(){
+  let text = "";
+  let possibleText = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for(let i = 0; i < 6; i++){
+  // for(let i = 0; i < (Math.floor(Math.random() * (7 - 1) + 7)); i++){
+    let randomLetter = Math.floor(Math.random() * (possibleText.length));
+    text += possibleText[randomLetter];
+    // return Math.random() * (max - min) + min;
+  }
+
+  // Advanced method:
+  // let text = "";
+  // for(let i = 0; i < 7; i++){
+  //   text += String.fromCharCode(Math.floor(Math.random() * ()))
+  // }
+  return text;
+}
+

c3744c914b7c7d100e59856d0e915586e333c7ee
Derrck Persson <derrickpersson@gmail.com>
2017-12-06 16:48:39 -0800

    Add register page with Static resource routing

--- express_server.js
+++ express_server.js
@@ -4,6 +4,23 @@ const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 
+function generateRandomString(){
+  let text = "";
+  let possibleText = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for(let i = 0; i < 6; i++){
+  // for(let i = 0; i < (Math.floor(Math.random() * (7 - 1) + 7)); i++){
+    let randomLetter = Math.floor(Math.random() * (possibleText.length));
+    text += possibleText[randomLetter];
+    // return Math.random() * (max - min) + min;
+  }
+
+  // Advanced method:
+  // let text = "";
+  // for(let i = 0; i < 7; i++){
+  //   text += String.fromCharCode(Math.floor(Math.random() * ()))
+  // }
+  return text;
+}
 
 app.set('view engine', 'ejs');
 
@@ -12,6 +29,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
   "abc" : "http://www.example.org"
 };
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+   "test": {
+    id: "test",
+    email: "test@example.com",
+    password: "test"
+  }
+}
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -47,9 +83,6 @@ app.get("/u/:shortURL", (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
 app.get('/urls/:id', function(req, res){
   let templateVars = {shortURL : req.params.id,
@@ -85,28 +118,6 @@ app.post('/register', function(req, res){
 
 });
 
-app.get('/register', function(req, res){
-  res.sendFile('public/register.html', { root : __dirname });
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-//  URL Shortening functions:
-
-function generateRandomString(){
-  let text = "";
-  let possibleText = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for(let i = 0; i < 6; i++){
-  // for(let i = 0; i < (Math.floor(Math.random() * (7 - 1) + 7)); i++){
-    let randomLetter = Math.floor(Math.random() * (possibleText.length));
-    text += possibleText[randomLetter];
-    // return Math.random() * (max - min) + min;
-  }
-
-  // Advanced method:
-  // let text = "";
-  // for(let i = 0; i < 7; i++){
-  //   text += String.fromCharCode(Math.floor(Math.random() * ()))
-  // }
-  return text;
-}
-
